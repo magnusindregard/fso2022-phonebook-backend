b91d7f7e7bbe62acc5be15e6c011784168ad9d4c
Magnus Indregard <magnus.indregard@knowit.no>
2022-12-29 17:36:42 +0100

    Added frontend build

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     { 
